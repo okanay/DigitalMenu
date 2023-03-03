efb8e3e06f520d1ede7ec9bf85a61153cb1ccff5
okanay <okanay@hotmail.com>
2023-03-03 12:08:25 +0300

    V0.3ColorTheme

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,9 @@ export default function Home() {
     }, [colorMode, darkMode])
 
     return (
-        <div className={`${theme} mx-auto max-w-screen-tablet mx-auto flex flex-col justify-start mt-4 text-sm`}>
+        <div className={`${theme} mx-auto max-w-screen-tablet mx-auto flex flex-col justify-start mt-4 text-sm bg-skin-theme-body-800`}>
 
-            <div className={'flex flex-col px-4 justify-between gap-2 py-2 tablet:flex-row bg-skin-theme-body-900'}>
+            <div className={'flex flex-col px-4 justify-between gap-2 py-2 tablet:flex-row'}>
 
                 <div className="flex flex-row gap-2">
                     <button
@@ -80,7 +80,7 @@ export default function Home() {
                 </div>
 
             </div>
-            <div className={`flex flex-col justify-start w-full bg-skin-theme-body-50 px-4 p-4 gap-2 pb-12`}>
+            <div className={`flex flex-col justify-start w-full bg-skin-theme-body-50 px-4 p-4 gap-2 pb-12 bg-skin-theme-body-50`}>
                 <h1 className={'text-3xl text-skin-theme-400 font-bold text-start'}>Color Theme</h1>
                 <p className={'text-sm text-skin-theme-font-50 font-semibold'}>Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. A accusantium dolor dolorem doloremque
@@ -96,7 +96,7 @@ export default function Home() {
                     </button>
                 </div>
             </div>
-            <div className={'grid grid-cols-1 grid-rows-1 tablet:grid-cols-2 tablet:gap-8 gap-4 mt-4 '}>
+            <div className={'grid grid-cols-1 grid-rows-1 tablet:grid-cols-2 tablet:gap-8 gap-4 pt-4 bg-skin-theme-body-50 px-4 py-8'}>
                 <div
                     className={'bg-skin-theme-body-50 border border-skin-theme-body-900 rounded-sm text-skin-theme-font-50 mx-auto'}>
                     <div className={'flex flex-col justify-start gap-4 px-4 py-4 max-w-screen-smPhone'}>
@@ -106,7 +106,7 @@ export default function Home() {
                         <h1 className={'text-4xl text-skin-theme-400 font-bold text-center mb-2'}>$29<span
                             className={'text-lg text-skin-theme-font-50'}> / month</span></h1>
 
-                        <div className={'flex flex-col gap-2 text-base text-start pl-4'}>
+                        <div className={'flex flex-col gap-2 text-sm text-start pl-4'}>
                             <p>Individual configuration</p>
                             <p>No setup, or hidden fees</p>
                             <p>Team size: <span>1 developer</span></p>
@@ -127,13 +127,12 @@ export default function Home() {
                 <div
                     className={'bg-skin-theme-body-50 border border-skin-theme-body-900 rounded-sm text-skin-theme-font-50 mx-auto'}>
                     <div className={'flex flex-col justify-start gap-4 px-4 py-4 max-w-screen-smPhone'}>
-                        <h2 className={'text-2xl text-skin-theme-400 font-semibold text-center'}>Starter</h2>
-                        <h3 className={'text-lg text-center mx-auto w-3/4'}>Best option for
-                            personal use & for your next project.</h3>
-                        <h1 className={'text-4xl text-skin-theme-400 font-bold text-center mb-2'}>$29<span
+                        <h2 className={'text-2xl text-skin-theme-400 font-semibold text-center'}>Enterprise</h2>
+                        <h3 className={'text-lg text-center mx-auto w-3/4'}>Best for large scale uses and extended redistribution rights.</h3>
+                        <h1 className={'text-4xl text-skin-theme-400 font-bold text-center mb-2'}>$249<span
                             className={'text-lg text-skin-theme-font-50'}> / month</span></h1>
 
-                        <div className={'flex flex-col gap-2 text-base text-start pl-4'}>
+                        <div className={'flex flex-col gap-2 text-sm text-start pl-4'}>
                             <p>Individual configuration</p>
                             <p>No setup, or hidden fees</p>
                             <p>Team size: <span>1 developer</span></p>
